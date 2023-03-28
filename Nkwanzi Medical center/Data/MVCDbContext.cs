4c729ab2ca1241c0e0ce3a420192a5265b8afbd4
Kayanja John <111996831+KayanjaJohn@users.noreply.github.com>
2023-03-28 23:36:17 +0300

    Add project files.

---
+++ Nkwanzi Medical center/Data/MVCDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nkwanzi_Medical_center.Models.Domain;
+
+namespace Nkwanzi_Medical_center.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
