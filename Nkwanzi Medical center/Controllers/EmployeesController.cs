76c60550617f597b612f9edc5053106b4faed1bc
Kayanja John <111996831+KayanjaJohn@users.noreply.github.com>
2023-03-29 11:06:19 +0300

    My progress

--- Nkwanzi Medical center/Controllers/EmployeesController.cs
+++ Nkwanzi Medical center/Controllers/EmployeesController.cs
@@ -1,13 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nkwanzi_Medical_center.Data;
 
 namespace Nkwanzi_Medical_center.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly MVCDbContext mvcDbContext;
+
+        public EmployeesController(MVCDbContext mvcDbContext)
+        {
+            this.mvcDbContext = mvcDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Models.AddEmployeeViewModel addEmployeeResult)
+        {
+            var employee = new Models.Domain.Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeResult.Name,
+                Email = addEmployeeResult.Email,
+                Salary = addEmployeeResult.Salary,
+                Department = addEmployeeResult.Department,
+                DateOfBirth = addEmployeeResult.DateOfBirth
+            };
+
+            await mvcDbContext.Employees.AddAsync(employee);
+            await mvcDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
         }
     }
 }
